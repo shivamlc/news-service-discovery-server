b6a16ef73db3b9ab331cd5aa38f7086da34464bd
shivamacshack <s.gaur@uq.edu.au>
2025-08-13 12:18:39 +1000

    Initial commit: Spring Boot Eureka Server project

---
+++ src/main/java/com/sgtech/news_eureka_server/NewsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.sgtech.news_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgtech/news_eureka_server/NewsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgtech.news_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
